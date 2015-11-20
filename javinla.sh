15982aa92ea9e93f7c06140100841a5f3a815426
Pavel Sorokin <pavel.sorokin@nerro.eu>
2015-11-20 10:56:27 +0100

    #6: Add java version 8u65

--- javinla.sh
+++ javinla.sh
@@ -23,6 +23,7 @@ java_versions[8u40]="http://download.oracle.com/otn-pub/java/jdk/8u40-b25/server
 java_versions[8u45]="http://download.oracle.com/otn-pub/java/jdk/8u45-b14/server-jre-8u45-linux-x64.tar.gz"
 java_versions[8u51]="http://download.oracle.com/otn/java/jdk/8u51-b16/server-jre-8u51-linux-x64.tar.gz"
 java_versions[8u60]="http://download.oracle.com/otn-pub/java/jdk/8u60-b27/server-jre-8u60-linux-x64.tar.gz"
+java_versions[8u65]="http://download.oracle.com/otn-pub/java/jdk/8u65-b17/server-jre-8u65-linux-x64.tar.gz"
 
 
 #------------------------------------------------------------------------------
